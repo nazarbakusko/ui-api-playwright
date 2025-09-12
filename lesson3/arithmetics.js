a833e2f8731dab0acd29b100d19ea605c0370ad2
nazarii baks <baksnaz@gmail.com>
2025-09-12 15:03:03 +0300

    Added arithmetics and logic files

---
+++ lesson3/arithmetics.js
@@ -0,0 +1,52 @@
+const a = 10;
+const b = 2;
+
+const c = 'Small';
+const d = 'pony';
+const e = '6';
+
+const boolTrue = true;
+const boolFalse = false;
+
+let undef;
+
+// Same types
+function numberNumber() {
+    console.log('Add:', a + b);
+    console.log('Multiply:', a * b);
+    console.log('Divide:', a / b);
+    console.log('Deduct:', a - b);
+    console.log('Pow:', a ** b);
+    console.log("num1 % num2:", a % b);
+}
+
+function stringString() {
+    console.log('Concat:', c + ' ' + d);
+}
+
+function booleanBoolean() {
+    console.log("Boolean add (true + false):", boolTrue + boolFalse);
+}
+
+numberNumber();
+stringString();
+booleanBoolean();
+
+//Different types
+
+// str - num
+console.log('str - num:', e - b);
+
+// str + num
+console.log('str + num:', e + b);
+
+// num + null
+console.log('num + null:', a + null);
+
+// num + undef
+console.log('num + bool:', a + undef);
+
+//
+const parsed = Number(e); // або parseInt(strNum1, 10)
+console.log("parsed e + a (explicit):", parsed + a)
+
